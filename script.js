0273a290a3c24323485b009e145e17bfebc3a5f1
JeeJee69 <livioauleta5@gmail.com>
2025-05-11 17:54:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let numeroClick = 0;
+const titoloOriginale = "Benvenuto su sito FAQ-School";
+
+function aggiornaFraseClick() {
+  const fraseClick = document.getElementById("fraseClick");
+  const contatore = document.getElementById("contatore");
+  contatore.textContent = numeroClick;
+}
+
+function cambiaTesto() {
+  numeroClick++;
+  const titolo = document.getElementById("titolo");
+  titolo.textContent = "perchè l'hai fatto?";
+  aggiornaFraseClick();
+}
+
+function resetTitolo() {
+  numeroClick++;
+  const titolo = document.getElementById("titolo");
+  titolo.textContent = titoloOriginale;
+  aggiornaFraseClick();
+}
