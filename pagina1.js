c9a98651d337abec274cfbb048a4082da7e7775f
JeeJee69 <livioauleta5@gmail.com>
2025-05-14 09:55:34 +0200

    Add files via upload

---
+++ pagina1.js
@@ -0,0 +1,5 @@
+function saluta() {
+  const risultato = document.getElementById("risultato");
+  risultato.textContent = "Ciao dalla pagina 1!";
+  console.log("Hai cliccato il pulsante su Pagina 1");
+}
